47d2ed5bfb59737d6beccc59d396ad83cd9ee45d
salilagidze <salagidze@gmail.com>
2025-12-24 16:52:54 +0400

    Initial Commit

---
+++ Final_Exam_Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Final_Exam_Movies
+//
+//  Created by Sali  on 24.12.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
