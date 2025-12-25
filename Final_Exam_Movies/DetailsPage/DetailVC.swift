a63a460f88dd37aae570fd2b74519888d1321f9b
salilagidze <salagidze@gmail.com>
2025-12-25 16:10:26 +0400

    DetailsPageDesign

--- Final_Exam_Movies/DetailsPage/DetailVC.swift
+++ Final_Exam_Movies/DetailsPage/DetailVC.swift
@@ -11,18 +11,21 @@ class DetailVC: UIViewController {
     
     let mainMovieTitleLabel = UILabel()
     let mainPoster = UIImageView()
-    let imdgIdButton = UIButton()
     let miniPoster = UIImageView()
     let movietitle = UILabel()
     let yearTitle = UILabel()
     let runTime = UILabel()
     let action = UILabel()
+    let divider = UIView()
     let aboutMovieLabel = UILabel()
     let favoriteButton = UIButton()
-    let isFavorite = false
-    let divider = UIView()
+    var isFavorite = false
+    let divider2 = UIView()
     let textAbout = UILabel()
     let infoStack = UIStackView()
+    let imdbRaitingView = UIView()
+    let ratingIcon = UIImageView()
+    let ratingLabel = UILabel()
     
     
     
@@ -30,35 +33,49 @@ class DetailVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .black
         setupTopView()
-       
+        setupBottomView()
+        
     }
     
     func setupTopView() {
-        [mainMovieTitleLabel, mainPoster, miniPoster, imdgIdButton, movietitle].forEach {
+        [mainMovieTitleLabel, mainPoster, miniPoster, imdbRaitingView, ratingIcon, ratingLabel, movietitle,].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             view.addSubview($0)
         }
         NSLayoutConstraint.activate([
             mainMovieTitleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
             mainMovieTitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-
+            
             mainPoster.topAnchor.constraint(equalTo: mainMovieTitleLabel.bottomAnchor, constant: 10),
             mainPoster.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             mainPoster.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             mainPoster.heightAnchor.constraint(equalToConstant: 200),
-
+            
             miniPoster.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-            miniPoster.topAnchor.constraint(equalTo: mainPoster.bottomAnchor, constant: -35),
+            miniPoster.topAnchor.constraint(equalTo: mainPoster.bottomAnchor, constant: -75),
             miniPoster.widthAnchor.constraint(equalToConstant: 110),
-            miniPoster.heightAnchor.constraint(equalToConstant: 130),
-
+            miniPoster.heightAnchor.constraint(equalToConstant: 150),
+            
             movietitle.topAnchor.constraint(equalTo: mainPoster.bottomAnchor, constant: 10),
             movietitle.leadingAnchor.constraint(equalTo: miniPoster.trailingAnchor, constant: 8),
             movietitle.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            
+            imdbRaitingView.bottomAnchor.constraint(equalTo: mainPoster.bottomAnchor, constant: -12),
+            imdbRaitingView.trailingAnchor.constraint(equalTo: mainPoster.trailingAnchor, constant: -12),
+            imdbRaitingView.heightAnchor.constraint(equalToConstant: 32),
+            
+            ratingIcon.leadingAnchor.constraint(equalTo: imdbRaitingView.leadingAnchor, constant: 8),
+            ratingIcon.centerYAnchor.constraint(equalTo: imdbRaitingView.centerYAnchor),
+            ratingIcon.widthAnchor.constraint(equalToConstant: 24),
+            ratingIcon.heightAnchor.constraint(equalToConstant: 24),
+            
+            ratingLabel.leadingAnchor.constraint(equalTo: ratingIcon.trailingAnchor, constant: 4),
+            ratingLabel.trailingAnchor.constraint(equalTo: imdbRaitingView.trailingAnchor, constant: -8),
+            ratingLabel.centerYAnchor.constraint(equalTo: imdbRaitingView.centerYAnchor),
+            
+            
         ])
         
-        
-
         mainMovieTitleLabel.textColor = .white
         mainMovieTitleLabel.textAlignment = .center
         mainMovieTitleLabel.text = "Movie Title"
@@ -75,29 +92,44 @@ class DetailVC: UIViewController {
         ]
         
         miniPoster.clipsToBounds = true
-        miniPoster.backgroundColor = .red
+        miniPoster.backgroundColor = .gray
         miniPoster.layer.cornerRadius = 20
         
         movietitle.textColor = .white
-        movietitle.font = .boldSystemFont(ofSize: 18)
+        movietitle.font = .boldSystemFont(ofSize: 22)
         movietitle.text = "Spider"
         
+        imdbRaitingView.backgroundColor = UIColor.black.withAlphaComponent(0.7)
+        imdbRaitingView.layer.cornerRadius = 10
+        
+        ratingIcon.image = UIImage(systemName: "star")
+        ratingIcon.tintColor = .systemOrange
+        
+        ratingLabel.text = "--"
+        ratingLabel.textColor = .systemOrange
+        ratingLabel.font = .systemFont(ofSize: 18)
+        
         infoStack.axis = .horizontal
-        infoStack.spacing = 20
+        infoStack.spacing = 15
+        infoStack.distribution = .fill
+        infoStack.alignment = .center
         infoStack.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(infoStack)
-
+        
         infoStack.addArrangedSubview(movieInfo(icon: "calendar", text: "2024"))
-        infoStack.addArrangedSubview(movieInfo(icon: "clock", text: "2h 15m"))
+        infoStack.addArrangedSubview(makeDivider())
+        infoStack.addArrangedSubview(movieInfo(icon: "clock", text: "145 Minutes"))
+        infoStack.addArrangedSubview(makeDivider())
         infoStack.addArrangedSubview(movieInfo(icon: "film", text: "Action"))
-
-
+        
+        
+        
         NSLayoutConstraint.activate([
-            infoStack.topAnchor.constraint(equalTo: movietitle.bottomAnchor, constant: 80),
-            infoStack.leadingAnchor.constraint(equalTo: miniPoster.trailingAnchor),
-            infoStack.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -20)
+            infoStack.topAnchor.constraint(equalTo: miniPoster.bottomAnchor, constant: 15),
+            infoStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            infoStack.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -15)
         ])
-
+        
     }
     
     func movieInfo(icon: String, text: String) -> UIView {
@@ -109,20 +141,91 @@ class DetailVC: UIViewController {
         iconImageView.contentMode = .scaleAspectFit
         iconImageView.tintColor = .lightGray
         iconImageView.translatesAutoresizingMaskIntoConstraints = false
-        iconImageView.widthAnchor.constraint(equalToConstant: 18).isActive = true
-        iconImageView.heightAnchor.constraint(equalToConstant: 18).isActive = true
+        iconImageView.widthAnchor.constraint(equalToConstant: 15).isActive = true
+        iconImageView.heightAnchor.constraint(equalToConstant: 15).isActive = true
         
         
         let textLabel = UILabel()
         textLabel.text = text
-        textLabel.textColor = UIColor.gray.withAlphaComponent(0.5)
-        textLabel.font = .systemFont(ofSize: 20)
+        textLabel.textColor = UIColor.gray.withAlphaComponent(0.6)
+        textLabel.font = .systemFont(ofSize: 17)
+        textLabel.numberOfLines = 1
         
         stack.addArrangedSubview(iconImageView)
         stack.addArrangedSubview(textLabel)
         
         return stack
-
+        
     }
-
+    
+    func makeDivider() -> UIView {
+        let divider = UIView()
+        divider.backgroundColor = .lightGray
+        divider.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            divider.heightAnchor.constraint(equalToConstant: 15),
+            divider.widthAnchor.constraint(equalToConstant: 1)
+        ])
+        
+        return divider
+    }
+    
+    func setupBottomView() {
+        [aboutMovieLabel, favoriteButton, divider2, textAbout].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+        NSLayoutConstraint.activate([
+            aboutMovieLabel.topAnchor.constraint(equalTo: infoStack.bottomAnchor, constant: 50),
+            aboutMovieLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            
+            favoriteButton.centerYAnchor.constraint(equalTo: aboutMovieLabel.centerYAnchor),
+            favoriteButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            favoriteButton.widthAnchor.constraint(equalToConstant: 50),
+            favoriteButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            divider2.topAnchor.constraint(equalTo: aboutMovieLabel.bottomAnchor, constant: 8),
+            divider2.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            divider2.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            divider2.heightAnchor.constraint(equalToConstant: 1),
+            
+            textAbout.topAnchor.constraint(equalTo: divider2.bottomAnchor, constant: 12),
+            textAbout.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            textAbout.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            
+        ])
+        
+        aboutMovieLabel.text = "About Movie"
+        aboutMovieLabel.font = .boldSystemFont(ofSize: 25)
+        aboutMovieLabel.textColor = .white
+        
+        favoriteButton.setImage(UIImage(systemName: "heart"), for: .normal)
+        favoriteButton.tintColor = .systemGray
+        favoriteButton.addTarget(self, action: #selector(favoritesTapped), for: .touchUpInside)
+        
+        divider2.backgroundColor = .systemGray
+        
+        textAbout.text = "ajafakjfbjavjsv"
+        textAbout.numberOfLines = 0
+        textAbout.textColor = .systemGray
+        
+    }
+    
+    @objc private func favoritesTapped() {
+        isFavorite.toggle()
+        
+        let imageName = isFavorite ? "heart.fill" : "heart"
+        favoriteButton.setImage(UIImage(systemName: imageName), for: .normal)
+        favoriteButton.tintColor = isFavorite ? .systemRed : .systemGray
+        
+        UIView.animate(withDuration: 0.2, animations: {
+            self.favoriteButton.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        }, completion: { _ in
+            UIView.animate(withDuration: 0.2) {
+                self.favoriteButton.transform = .identity
+            }
+            
+        })
+    }
 }
